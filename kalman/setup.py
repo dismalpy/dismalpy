8c523c7920d50ef8c919a3f5466da2654d7092c5
Chad Fulton <chad@chadfulton.com>
2014-03-14 12:06:58 -0700

    CLEAN: Removed obsolete lines.

--- kalman/setup.py
+++ kalman/setup.py
@@ -5,23 +5,12 @@ from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 from numpy.distutils.system_info import get_info
 
-#blas_include = get_info('blas_opt')['extra_compile_args'][1][2:]
-#includes = [blas_include,numpy.get_include()]
-
-#ext_modules = cythonize([
-#    Extension("cylapack", ["cylapack.pyx"],
-#              include_dirs = includes,
-#              libraries=['blas','lapack']),
-#    Extension("kalman_filter", ["kalman_filter.pyx"])
-#])
 ext_modules = [
     Extension("kalman_filter", ["kalman_filter.pyx"]),
-    Extension("kalman_smoother", ["kalman_smoother.pyx"]),
-    Extension("dkalman_filter", ["dkalman_filter.pyx"]),
 ]
 
 setup(
-  name = 'Markov Switching Model Tools',
+  name = 'Multivariate Kalman Filter',
   cmdclass = {'build_ext': build_ext},
   ext_modules = ext_modules,
   include_dirs=[numpy.get_include()]
