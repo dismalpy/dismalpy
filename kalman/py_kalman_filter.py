6de2993b81c71f961359bb8688313771a3a6934c
Chad Fulton <chad@chadfulton.com>
2014-05-14 14:30:54 -0700

    REF: pep8 fixes

--- kalman/py_kalman_filter.py
+++ kalman/py_kalman_filter.py
@@ -8,12 +8,13 @@ License: Simplified-BSD
 
 import numpy as np
 
-def py_kalman_filter(y,  # T+1xn    (data: endogenous, observed)
-                     H,  # nxkxT+1  (parameters)
-                     mu, # kx1      (parameters)
-                     F,  # kxk      (parameters)
-                     R,  # kxk      (parameters: covariance matrix)
-                     Q,  # kxk      (parameters: covariance matrix)
+
+def py_kalman_filter(y,   # T+1xn    (data: endogenous, observed)
+                     H,   # nxkxT+1  (parameters)
+                     mu,  # kx1      (parameters)
+                     F,   # kxk      (parameters)
+                     R,   # kxk      (parameters: covariance matrix)
+                     Q,   # kxk      (parameters: covariance matrix)
                      z=None,  # T+1xr    (data: weakly exogenous, observed)
                      A=None,  # nxr      (parameters)
                      beta_tt_init=None,
@@ -29,43 +30,45 @@ def py_kalman_filter(y,  # T+1xn    (data: endogenous, observed)
         r = z.shape[1]
     else:
         r = 0
-    
+
     # Allocate memory for variables
-    beta_tt = np.zeros((T+1,k))
-    P_tt = np.zeros((T+1,k,k))
-    beta_tt1 = np.zeros((T+1,k))
-    P_tt1 = np.zeros((T+1,k,k))
-    y_tt1 = np.zeros((T+1,n))
-    eta_tt1 = np.zeros((T+1,n))
-    f_tt1 = np.zeros((T+1,n,n))
-    gain = np.zeros((T+1,k,n))
+    beta_tt = np.zeros((T+1, k))
+    P_tt = np.zeros((T+1, k, k))
+    beta_tt1 = np.zeros((T+1, k))
+    P_tt1 = np.zeros((T+1, k, k))
+    y_tt1 = np.zeros((T+1, n))
+    eta_tt1 = np.zeros((T+1, n))
+    f_tt1 = np.zeros((T+1, n, n))
+    gain = np.zeros((T+1, k, n))
     ll = np.zeros((T+1,))
-    
+
     # Initial values
     if beta_tt_init is None:
         beta_tt[0] = np.linalg.inv(np.eye(k) - F).dot(mu)
     else:
         beta_tt[0] = beta_tt_init
     if P_tt_init is None:
-        P_tt[0] = np.linalg.inv(np.eye(k**2) - np.kron(F,F)).dot(Q.reshape(Q.size, 1)).reshape(k,k)
+        P_tt[0] = np.linalg.inv(np.eye(k**2) - np.kron(F, F)).dot(
+            Q.reshape(Q.size, 1)
+        ).reshape(k, k)
     else:
         P_tt[0] = P_tt_init
 
     # Iterate forwards
-    H_view = H[:,:,0]
-    for t in range(1,T+1):
+    H_view = H[:, :, 0]
+    for t in range(1, T+1):
         if time_varying_H:
-            H_view = H[:,:,t]
-        
+            H_view = H[:, :, t]
+
         # Prediction
         beta_tt1[t] = mu + F.dot(beta_tt[t-1])
-        P_tt1[t]    = F.dot(P_tt[t-1]).dot(F.T) + Q
-        y_tt1[t]    = H_view.dot(beta_tt1[t])
+        P_tt1[t] = F.dot(P_tt[t-1]).dot(F.T) + Q
+        y_tt1[t] = H_view.dot(beta_tt1[t])
         if r > 0:
             y_tt1[t] += A.dot(z[t-1])
-        eta_tt1[t]  = y[t-1] - y_tt1[t]
+        eta_tt1[t] = y[t-1] - y_tt1[t]
         PHT = P_tt1[t].dot(H_view.T)
-        f_tt1[t]    = H_view.dot(PHT) + R
+        f_tt1[t] = H_view.dot(PHT) + R
         if n == 1:
             f_inv = 1/f_tt1[t]
             det = np.abs(f_tt1[t])
@@ -74,11 +77,14 @@ def py_kalman_filter(y,  # T+1xn    (data: endogenous, observed)
             det = np.linalg.det(f_tt1[t])
 
         # Log-likelihood as byproduct
-        ll[t] = -0.5*np.log(2*np.pi*det) - 0.5*eta_tt1[t].T.dot(f_inv).dot(eta_tt1[t])
-        
+        ll[t] = (
+            - 0.5*np.log(2*np.pi*det)
+            - 0.5*eta_tt1[t].T.dot(f_inv).dot(eta_tt1[t])
+        )
+
         # Updating
         gain[t] = PHT.dot(f_inv)
         beta_tt[t] = beta_tt1[t] + gain[t].dot(eta_tt1[t])
         P_tt[t] = P_tt1[t] - gain[t].dot(H_view).dot(P_tt1[t])
-    
+
     return beta_tt, P_tt, beta_tt1, P_tt1, y_tt1, eta_tt1, f_tt1, gain, ll
