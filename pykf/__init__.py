79613d4e04a19b19cdae2bc14816972fe655aad6
Chad Fulton <chad@chadfulton.com>
2014-07-14 12:49:47 -0700

    ENH: Added Python statespace representation class.

--- pykf/__init__.py
+++ pykf/__init__.py
@@ -5,3 +5,38 @@ try:
     from .version import version as __version__
 except ImportError:
     __version__ = 'not-yet-built'
+
+from .representation import (
+    Representation,
+
+    FILTER_CONVENTIONAL,
+    FILTER_EXACT_INITIAL,
+    FILTER_AUGMENTED,
+    FILTER_SQUARE_ROOT,
+    FILTER_UNIVARIATE,
+    FILTER_COLLAPSED,
+    FILTER_EXTENDED,
+    FILTER_UNSCENTED,
+
+    INVERT_UNIVARIATE,
+    SOLVE_LU,
+    INVERT_LU,
+    SOLVE_CHOLESKY,
+    INVERT_CHOLESKY,
+
+    STABILITY_FORCE_SYMMETRY,
+
+    MEMORY_STORE_ALL,
+    MEMORY_NO_FORECAST,
+    MEMORY_NO_PREDICTED,
+    MEMORY_NO_FILTERED,
+    MEMORY_NO_LIKELIHOOD,
+    MEMORY_CONSERVE
+)
+
+from .tools import (
+    find_best_blas_type, prefix_dtype_map,
+    prefix_statespace_map, prefix_kalman_filter_map,
+
+    constrain_stationary, unconstrain_stationary,
+)
