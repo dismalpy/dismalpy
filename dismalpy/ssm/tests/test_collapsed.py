f4608ab0fa7b865cdc09f83fd69ca0513fd79363
Chad Fulton <chad@chadfulton.com>
2014-11-16 13:21:44 -0800

    TST: Added tests for the missing / collapsed case

--- dismalpy/ssm/tests/test_collapsed.py
+++ dismalpy/ssm/tests/test_collapsed.py
@@ -15,7 +15,7 @@ import os
 
 from dismalpy import ssm
 import dismalpy.ssm.tests.results_kalman as results_kalman_filter
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_allclose, assert_allclose
 from nose.exc import SkipTest
 
 current_path = os.path.dirname(os.path.abspath(__file__))
@@ -58,109 +58,110 @@ class Trivariate(ssm.Representation):
 
     @SkipTest
     def test_forecasts(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.forecasts[0,:],
-            self.results_b.forecasts[0,:], 9
+            self.results_b.forecasts[0,:]
         )
 
     @SkipTest
     def test_forecasts_error(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.forecasts_error[0,:],
-            self.results_b.forecasts_error[0,:], 9
+            self.results_b.forecasts_error[0,:]
         )
 
     @SkipTest
     def test_forecasts_error_cov(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.forecasts_error_cov[0,0,:],
-            self.results_b.forecasts_error_cov[0,0,:], 9
+            self.results_b.forecasts_error_cov[0,0,:]
         )
 
     def test_filtered_state(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.filtered_state,
-            self.results_b.filtered_state, 8
+            self.results_b.filtered_state
         )
 
     def test_filtered_state_cov(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.filtered_state_cov,
-            self.results_b.filtered_state_cov, 9
+            self.results_b.filtered_state_cov
         )
 
     def test_predicted_state(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.predicted_state,
-            self.results_b.predicted_state, 8
+            self.results_b.predicted_state
         )
 
     def test_predicted_state_cov(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.predicted_state_cov,
-            self.results_b.predicted_state_cov, 9
+            self.results_b.predicted_state_cov
         )
 
     def test_loglike(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.loglikelihood,
-            self.results_b.loglikelihood, 7
+            self.results_b.loglikelihood
         )
 
     def test_smoothed_states(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.smoothed_state,
-            self.results_b.smoothed_state, 8
+            self.results_b.smoothed_state
         )
 
     def test_smoothed_states_cov(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.smoothed_state_cov,
-            self.results_b.smoothed_state_cov, 5
+            self.results_b.smoothed_state_cov,
+            rtol=1e-4
         )
 
     @SkipTest
     def test_smoothed_measurement_disturbance(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.smoothed_measurement_disturbance,
-            self.results_b.smoothed_measurement_disturbance, 9
+            self.results_b.smoothed_measurement_disturbance
         )
 
     @SkipTest
     def test_smoothed_measurement_disturbance_cov(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.smoothed_measurement_disturbance_cov,
-            self.results_b.smoothed_measurement_disturbance_cov, 9
+            self.results_b.smoothed_measurement_disturbance_cov
         )
 
     def test_smoothed_state_disturbance(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.smoothed_state_disturbance,
-            self.results_b.smoothed_state_disturbance, 9
+            self.results_b.smoothed_state_disturbance
         )
 
     def test_smoothed_state_disturbance_cov(self):
-        assert_almost_equal(
+        assert_allclose(
             self.results_a.smoothed_state_disturbance_cov,
-            self.results_b.smoothed_state_disturbance_cov, 9
+            self.results_b.smoothed_state_disturbance_cov
         )
 
     def test_simulation_smoothed_state(self):
-        assert_almost_equal(
+        assert_allclose(
             self.sim_a.simulated_state,
-            self.sim_a.simulated_state, 9
+            self.sim_a.simulated_state
         )
 
     def test_simulation_smoothed_measurement_disturbance(self):
-        assert_almost_equal(
+        assert_allclose(
             self.sim_a.simulated_measurement_disturbance,
-            self.sim_a.simulated_measurement_disturbance, 9
+            self.sim_a.simulated_measurement_disturbance
         )
 
     def test_simulation_smoothed_state_disturbance(self):
-        assert_almost_equal(
+        assert_allclose(
             self.sim_a.simulated_state_disturbance,
-            self.sim_a.simulated_state_disturbance, 9
+            self.sim_a.simulated_state_disturbance
         )
 
 class TestTrivariateConventional(Trivariate):
@@ -185,10 +186,111 @@ class TestTrivariateConventional(Trivariate):
             initial_state_variates=np.zeros(self.k_states)
         )
 
+class TestTrivariateConventionalPartialMissing(Trivariate):
+
+    def __init__(self, dtype=float, **kwargs):
+        super(TestTrivariateConventionalPartialMissing, self).__init__(dtype, **kwargs)
+        n_disturbance_variates = (self.k_endog + self.k_posdef) * self.nobs
+
+        # Set partially missing data
+        self.endog[:2, 10:180] = np.nan
+
+        # Conventional filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_CONVENTIONAL
+        self.results_a = self.smooth()
+        self.sim_a = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+        # Univariate filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_CONVENTIONAL | ssm.FILTER_COLLAPSED
+        self.results_b = self.smooth()
+        self.sim_b = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+class TestTrivariateConventionalAllMissing(Trivariate):
+
+    def __init__(self, dtype=float, **kwargs):
+        super(TestTrivariateConventionalAllMissing, self).__init__(dtype, **kwargs)
+        n_disturbance_variates = (self.k_endog + self.k_posdef) * self.nobs
+
+        # Set partially missing data
+        self.endog[:, 10:180] = np.nan
+
+        # Conventional filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_CONVENTIONAL
+        self.results_a = self.smooth()
+        self.sim_a = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+        # Univariate filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_CONVENTIONAL | ssm.FILTER_COLLAPSED
+        self.results_b = self.smooth()
+        self.sim_b = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+
 class TestTrivariateUnivariate(Trivariate):
 
     def __init__(self, dtype=float, **kwargs):
         super(TestTrivariateUnivariate, self).__init__(dtype, **kwargs)
+
+        # Conventional filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_UNIVARIATE
+        self.results_a = self.smooth()
+        n_disturbance_variates = (self.k_endog + self.k_posdef) * self.nobs
+        self.sim_a = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+        # Univariate filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_UNIVARIATE | ssm.FILTER_COLLAPSED
+        self.results_b = self.smooth()
+        self.sim_b = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+class TestTrivariateUnivariatePartialMissing(Trivariate):
+
+    def __init__(self, dtype=float, **kwargs):
+        super(TestTrivariateUnivariatePartialMissing, self).__init__(dtype, **kwargs)
+
+        # Set partially missing data
+        self.endog[:2, 10:180] = np.nan
+
+        # Conventional filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_UNIVARIATE
+        self.results_a = self.smooth()
+        n_disturbance_variates = (self.k_endog + self.k_posdef) * self.nobs
+        self.sim_a = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+        # Univariate filtering, smoothing, and simulation smoothing
+        self.filter_method = ssm.FILTER_UNIVARIATE | ssm.FILTER_COLLAPSED
+        self.results_b = self.smooth()
+        self.sim_b = self.simulation_smoother(
+            disturbance_variates=np.zeros(n_disturbance_variates),
+            initial_state_variates=np.zeros(self.k_states)
+        )
+
+class TestTrivariateUnivariateAllMissing(Trivariate):
+
+    def __init__(self, dtype=float, **kwargs):
+        super(TestTrivariateUnivariateAllMissing, self).__init__(dtype, **kwargs)
+
+        # Set partially missing data
+        self.endog[:, 10:180] = np.nan
 
         # Conventional filtering, smoothing, and simulation smoothing
         self.filter_method = ssm.FILTER_UNIVARIATE
