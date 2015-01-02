84c68dec5e1f4ed6a79290ddf07cadf0918a573d
Chad Fulton <chad@chadfulton.com>
2015-01-02 12:55:56 -0800

    CLEAN: Improve docblocks, inline comments, clean.

--- dismalpy/model.py
+++ dismalpy/model.py
@@ -10,6 +10,8 @@ import pandas as pd
 
 class Model(object):
     """
+    Model
+
     `endog` is one of:
     - a name (str)
         k_endog = 1
@@ -39,29 +41,39 @@ class Model(object):
         nobs = dataframe.shape[0]
         names = series.columns
     """
-    def __init__(self, endog, nobs=None, *args, **kwargs):
-        self.endog = None
 
+    endog = None
+    nobs = None
+    _dates = None
+    
+    def __init__(self, endog, nobs=None, *args, **kwargs):
+        # Single endogenous variable; name provided
         if type(endog) == str:
             self._endog_names = [endog]
             self.k_endog = 1
             self.nobs = None
             endog = None
+        # Many endogenous variables; names and data (pandas DataFrame) provided
         elif isinstance(endog, pd.DataFrame):
             self._endog_names = endog.columns.tolist()
             self.nobs, self.k_endog = endog.shape
+        # Single endogenous variable; names and data (pandas Series) provided
         elif isinstance(endog, pd.Series):
             self._endog_names = [endog.name] if endog.name is not None else [id(endog)]
             self.k_endog = 1
             self.nobs = endog.shape[0]
+        # Other provided; assumed to be an iterable
         else:
-            if not isinstance(endog, np.ndarray):
-                endog = np.array(endog)
+            # Coerce to an array
+            endog = np.asarray(endog)
+
+            # Many endogenous variables; names provided
             if np.issubdtype(endog.dtype, str):
                 self._endog_names = list(endog)
                 self.k_endog = len(self._endog_names)
                 self.nobs = None
                 endog = None
+            # Many endogenous variables; data provided, names auto-generated
             else:
                 endog_id = id(endog)
                 if endog.ndim == 1:
@@ -84,14 +96,29 @@ class Model(object):
         if endog is not None:
             self.bind(endog)
 
-    def bind(self, endog):
+    def bind(self, endog, long_format=True):
         """
         Bind endogenous data to this instance
+
+        Parameters
+        ----------
+        endog : array_like
+            Array of endogenous data.
+        long_format : boolean
+            Whether or not the array is in long format (nobs x k_endog)
+
+        Notes
+        -----
+        This method sets the `endog`, `nobs`, and possibly the `_dates`
+        attributes.
+
+        After the call, `self.endog` will be a 
         """
-        # If we were given a Pandas object, make sure the names match
-        # and optionally extract only those columns we need
-        if isinstance(endog, pd.Series) and endog.name is not None:
-            if not endog.name == self._endog_names[0]:
+        # If we were given a Pandas object, check for:
+        # - Names match
+        # - Date index
+        if isinstance(endog, pd.Series):
+            if endog.name is not None and not endog.name == self._endog_names[0]:
                 raise ValueError('Name of the provided endogenous array does'
                                  ' not match the given endogenous name.'
                                  ' Got %s, required %s'
@@ -109,13 +136,14 @@ class Model(object):
             self._dates = endog.index
 
         # Explicitly copy / convert to a new ndarray
-        # Note: we assume that the given endog array is nobs x k_endog, but
-        # _statespace assumes it is k_endog x nobs. Thus we create it in the
-        # transposed shape as order "C" and then transpose to get order "F".
-        if np.ndim(endog) == 1:
-            endog = np.array(endog, ndmin=2, order="F")
+        # Note: typically the given endog array is in long format
+        # (nobs x k_endog), but _statespace assumes it is in wide format
+        # (k_endog x nobs). Thus we create the array in long format as order
+        # "C" and then transpose to get order "F".
+        if np.ndim(endog) == 1 or not long_format:
+            endog = np.array(endog, ndmin=2, order="F", copy=True)
         else:
-            endog = np.array(endog, order="C").T
+            endog = np.array(endog, order="C", copy=True).T
 
         # Check that this fits the k_endog dimension that we previously had
         if not endog.shape[0] == self.k_endog:
