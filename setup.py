a79ef3f4a7f79724294cd1ef4bc51639dbe384c7
Chad Fulton <chad@chadfulton.com>
2015-09-21 17:43:44 -0700

    BUILD: Bump version.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ Operating System :: MacOS
 
 MAJOR               = 0
 MINOR               = 2
-MICRO               = 1
+MICRO               = 2
 ISRELEASED          = True
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
